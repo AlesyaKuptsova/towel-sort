3d6d98ce2512614bb5783f405b3b3cd775ad7176
Alesia Kuptsova <alesya.kuptsova@tut.by>
2020-10-07 22:34:05 +0200

    feat: implement the sort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+    array = new Array();
+    if (matrix === undefined) {
+        return array;
+    }
+    for (i=0; i<matrix.length; i++){
+        if(i % 2 === 0){
+            for(j=0; j<matrix[i].length; j++){
+                array.push(matrix[i][j]);
+            }
+        } else {
+            for(j=matrix[i].length; j>0; j--){
+                array.push(matrix[i][j-1]);
+            }
+        }
+    }
+    return array;
 }
